f809789dbde157b3690dfa107a3e0ffc3e29d657
Kar_Yan <102352076+Karlenjak@users.noreply.github.com>
2023-02-13 23:43:29 +0100

    provas de uso de video

--- components/pageTitleCard/component.php
+++ components/pageTitleCard/component.php
@@ -1,8 +1,24 @@
-<div class="pageTitleCardContainer">
+<!-- <div class="pageTitleCardContainer">
     <div class="opacityContainer">
 
     </div>
     <div class="pageTitleCardText">
         <h2>Departament de daw</h2>
+    </div>
+</div> -->
+
+<style>
+  <?php include __DIR__ . '/template.css'; ?>
+</style>
+
+<div class="video-container">
+<video autoplay loop muted>
+<source src="./video/Group of Friends Toasting with Beer.mp4" type="video/mp4">
+</video>
+<!-- <video width="320" height="240" controls>
+    <source src="./video/Group of Friends Toasting with Beer.mp4" type="video/mp4">
+</video> -->
+    <div class="video-texto">
+      <h2>Cerveza</h2>
     </div>
 </div>
