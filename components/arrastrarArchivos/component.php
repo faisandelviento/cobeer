65ea1f8c29f6da4e88415e9b57a5568636c3a946
bryansint <bryansanchezm1k@gmail.com>
2023-02-15 00:31:18 +0100

    componente arrastrar

---
+++ components/arrastrarArchivos/component.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+    <title>Subir archivo</title>
+    <link rel="stylesheet" href="template.css" />
+</head>
+<body>
+    <div>
+        <div class="areaArrastre">
+            <h2>Arrastrar imagenes</h2>
+            <span>o</span>
+            <button>Selecciona tus archivos</button>
+            <input type="file" name="" id="inputImagen" hidden multiple />
+        </div>
+        <div id="viewArrastre"></div>   
+        <div id="viewCarpeta"></div>     
+    </div>
+    
+    <script src="script.js">
+    </script>  
+</body>
+</html>
