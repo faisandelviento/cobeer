177c188cd377b22eb30f37a296a8571f783fca64
MounirEJH <ejhmounir@gmail.com>
2023-02-15 17:43:09 +0100

    Login Mounir

--- view/pages/login/index.php
+++ view/pages/login/index.php
@@ -7,8 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../styles/layout.css">
     <link rel="stylesheet" href="../../styles/globals.css">
-    <link rel="stylesheet" href="stylesControl.css">
+    <!-- <link rel="stylesheet" href="stylesControl.css"> -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Catamaran' rel='stylesheet'>
     <title>Login</title>
     <script src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js"></script>
 </head>
@@ -22,11 +23,11 @@
         <main>
             <!-- FORMULARIO LOGIN -->
             <!-- <div class="centeredContainer">
-                <?php include '../../../components/loginForm/component.php';?>
+                <?php //include '../../../components/loginForm/component.php';?>
             </div> -->
             <!-- Login Yassine MOVER A COMPONENTE -->
-            <div class="login">
-                <h1>Login</h1>
+            <!-- <div class="login">
+                 <h1>Login</h1>
                 <div class="usuario">
                     <input class="input" type="text" placeholder="usuario">
                     <input class="input" type="text" placeholder="contraseña">
@@ -34,8 +35,11 @@
                     <a href="#">
                         <p>¿Olvidó su contraseña?</p>
                     </a>
-                </div>
-            </div>  
+                </div> 
+                <?php // include "components/loginForm/component.php" ?>
+
+            </div>   -->
+            <?php include "../../../components/loginForm/component.php" ?>
         </main>
 
         <footer>
