635075f9082c3193379bdbbcdc15ecf316b1d707
bmever1 <94168549+bmever1@users.noreply.github.com>
2023-02-22 17:21:58 +0100

    BD

--- components/articleCard/component.php
+++ components/articleCard/component.php
@@ -1,6 +1,6 @@
 <style>
-<?php include __DIR__ . '/template.css';
-?>
+    <?php include __DIR__ . '/template.css';
+    ?>
 </style>
 <?php include __DIR__ . '/controller.php'; ?>
 
@@ -10,13 +10,14 @@
 
     <div class="defaultCardText">
         <div class="defaultCardTitle">
-            <h1>Lorem Ipsum</h1>
+            <h1>
+                <?= $articulo['titulo']; ?>
+
+            </h1>
         </div>
         <div class="defaultCardDescription">
             <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since
-                the 1500s, when an ...
+                <?= $articulo['descripcion']; ?>
             </p>
         </div>
     </div>
