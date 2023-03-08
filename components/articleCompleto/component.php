ab17f4509dc189907cd916c25499989599be0331
MounirEJH <ejhmounir@gmail.com>
2023-03-08 16:23:17 +0100

    Login&Working in component articleCompleto

--- components/articleCompleto/component.php
+++ components/articleCompleto/component.php
@@ -1,21 +1,32 @@
 <style>
-    <?php include __DIR__ . '/template.css'; ?>
+  <?php include __DIR__ . '/template.css'; ?>
 </style>
 
 <article class="post">
-  <div>
-    <div class="absolute-bg" style="background-image: url('https://www.tooltyp.com/wp-content/uploads/2014/10/1900x920-8-beneficios-de-usar-imagenes-en-nuestros-sitios-web.jpg');"></div>
-  </div>
+
+  <section class="awSlider">
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src=<div id="carousel">
+        <img src="../../assets/imagenes/imgp.jpg" alt="Imagen 1">
+        <img src="../../assets/imagenes/imgp.jpg" alt="Imagen 2">
+        <img src="../../assets/imagenes/imgp.jpg" alt="Imagen 3">
+        <img src="../../assets/imagenes/imgp.jpg" alt="Imagen 4">
+      </div>
+      <button id="prevBtn">Anterior</button>
+      <button id="nextBtn">Siguiente</button>
+  </section>
+
   <div class="post__container">
     <span class="post__category">Departamento</span>
-    
+
     <div class="post__content">
       <header>
         <time class="post__time">Agosto 22 2023</time>
         <h1 class="post__header"><span>Shift the overall look </span> <span>and feel by adding these</span> <span>wonderful
             touches to furniture in your home</span></h1>
       </header>
-    
+
       <p class="post__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a augue justo. In mollis erat in elit tempus, feugiat luctus ex sollicitudin. Maecenas euismod tortor dolor, vel blandit augue aliquam sit amet. Vestibulum et eros mollis, laoreet nisi ac, condimentum sapien. Aliquam nec nunc enim.
         Lorem ipsum dolor sit amet consectetur, Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius, sed. Similique explicabo recusandae dolorum molestias blanditiis harum cupiditate repellendus quisquam, modi commodi incidunt facilis voluptatum totam accusantium ipsa eius quod tempore aspernatur suscipit nam, natus sint. Quis, temporibus, adipisci reiciendis quaerat magni rerum ab doloremque iure ducimus provident tempora omnis fugit, nobis incidunt. Nisi corporis repudiandae dolor cumque fugiat optio expedita doloribus nemo temporibus porro, minima reiciendis perferendis tempore tenetur eveniet neque. Odit vero facilis similique exercitationem unde dolorum, tempora sint inventore laborum doloribus voluptate numquam nostrum quis vitae eius fugit modi eos minima quasi minus quibusdam? Quisquam vero, quis, laboriosam nulla facere amet animi obcaecati repellat natus magni voluptatum. Atque, tempore excepturi sit vero qui dolores architecto libero vel distinctio voluptatibus illo, nostrum sequi error ut dignissimos laudantium ex quos pariatur deserunt nemo praesentium veniam, inventore eos. Excepturi at unde ullam optio deleniti pariatur harum quo odit quidem dolorum! adipisicing elit. Rem, dolore voluptatem ad totam tempore soluta veniam nulla repellendus exercitationem commodi consectetur nemo tenetur numquam libero. Eligendi tenetur id eveniet officiis eius in iusto. Necessitatibus itaque dicta totam excepturi tempora aliquid in facere magni atque impedit hic perspiciatis labore, magnam libero? Earum nulla odio sed quis. Nesciunt reprehenderit laborum quaerat vitae. Iste sit consequatur, explicabo sint iure architecto enim ad, delectus necessitatibus non voluptate laborum ut saepe harum error accusantium quia est esse facilis. Aut beatae dolore, cumque praesentium neque voluptate repellendus quo adipisci laborum sapiente possimus ea porro ab reiciendis!
       </p>
@@ -24,9 +35,9 @@
 
         <div class="avatar"></div>
         <div class="user-detail">
-        <h2 class="name">Yassine Kerzazi</h2>
+          <h2 class="name">Yassine Kerzazi</h2>
         </div>
-      </div> 
+      </div>
     </div>
   </div>
 </article>
