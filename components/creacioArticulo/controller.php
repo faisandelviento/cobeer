6d48a44a2feefefe1cb697e1849f3db2d7f3027b
PascualFort <fortpascual@gmail.com>
2023-03-08 16:42:29 +0100

    Prepared recursos inserts

--- components/creacioArticulo/controller.php
+++ components/creacioArticulo/controller.php
@@ -1,10 +1,13 @@
 <?php
 include __DIR__ . '/../../DB/classes/Articulo.php';
 include __DIR__ . '/../../services/storage/index.php';
-
 if (isset($_POST['enviar'])) {
     try {
-        storeFile($_FILES["fileToUpload"]);
+        $paths = storeFiles($_FILES["fileToUpload"]);
+        foreach ($paths as $path) {
+            echo $path;
+            //indsert de los recursos
+        }
     } catch (Exception $e) {
 
         $errores = "Hay que revisar esos puntos : <br> " . $e->getMessage();
