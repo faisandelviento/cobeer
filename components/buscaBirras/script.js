c5b25752609c3bb1fb330cd7f29a469bbbffd744
joelforworks <101220133+joelforworks@users.noreply.github.com>
2023-03-08 18:11:03 +0100

    Buscar birras (queda win/los funcionalidad)

--- components/buscaBirras/script.js
+++ components/buscaBirras/script.js
@@ -3,7 +3,10 @@
 import {
 	createBoard,
 	markTile,
-	revealTile
+	revealTile,
+	TILE_STATUSES,
+	checkWin,
+	checkLose
 } from './minesweeper.js';
 
 const BOARD_SIZE = 10;
@@ -12,8 +15,12 @@ const NUMBER_MINES = 10;
 const board = createBoard(10,10);
 
 const boardElement = document.querySelector('.minesweeper');
-const mineCount = document.querySelector('[data-mine-count]');
-mineCount.textContent=NUMBER_MINES
+const mineCountElement = document.querySelector('[data-mine-count]');
+mineCountElement.textContent= NUMBER_MINES;
+const minesCountMarked = {};
+
+const endMessage = document.querySelector('[end-message]');
+
 // make size of tiles 
 boardElement.style.setProperty('--size',BOARD_SIZE);
 
@@ -25,6 +32,7 @@ board.forEach( row =>{
 		'click',
 			()=>{
 				revealTile(board,tile);
+				checkGameEnd();
 			}
 	  );
 		tile.element.addEventListener(
@@ -32,10 +40,51 @@ board.forEach( row =>{
 			e => {
 				e.preventDefault();
 				markTile(tile);
-				mineCount.textContent--;
+				minesCount(tile,minesCountMarked);
 			}
 		);
 		;
 	});
 })
-console.log(board);
+
+function minesCount(tile,minesCountMarked){
+	if(minesCountMarked[`${tile.x}${tile.y}`]){
+		delete minesCountMarked[`${tile.x}${tile.y}`];
+	}else{
+		minesCountMarked[`${tile.x}${tile.y}`]=true;
+	}
+	mineCountElement.textContent = 
+		NUMBER_MINES - Object.keys(minesCountMarked).length < 0
+			? 0
+			: NUMBER_MINES - Object.keys(minesCountMarked).length;
+}
+
+function checkGameEnd(){
+	const win = checkWin(board);
+	const lose = checkLose(board);
+	
+	if(win || lose){
+		boardElement.addEventListener('click',stopProp,{capture:true});
+		boardElement.addEventListener('contextmenu',stopProp,{capture:true});
+	}
+
+	if(win){
+		endMessage.textContent="YOU WIN";
+	}
+	if(lose){
+		endMessage.textContent="YOU LOSE";
+		board.forEach(row=>{
+			row.forEach(tile=>{
+				if(tile.mine)revealTile(board,tile);
+			})
+		})
+	}
+
+}
+
+
+function stopProp(e){
+	e.stopImmediatePropagation();
+}
+
+
